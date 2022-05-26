209b2a73e1c52b85f095d0f6144a52fcc682ad62
kzeng24 <kathyzeng2002@gmail.com>
2022-05-26 16:48:32 -0400

    katherine commit

--- app/src/main/java/space/zengk/finalproject/Home.java
+++ app/src/main/java/space/zengk/finalproject/Home.java
@@ -59,6 +59,10 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        
+
+        return rootView;
     }
 }
